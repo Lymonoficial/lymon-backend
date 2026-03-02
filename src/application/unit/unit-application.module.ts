f7510fcd5db8727b74e8d3a578c5ac82edb11f1c
JaaysCode <jpcardonab0520@gmail.com>
2026-03-02 10:19:05 -0500

    feat(unit): Implement GetUnitsByProperty query and corresponding handler with pagination

--- src/application/unit/unit-application.module.ts
+++ src/application/unit/unit-application.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CreateUnitHandler } from '@/application/unit/commands/create-unit.handler';
+import { GetUnitsByPropertyQueryHandler } from '@/application/unit/queries/GetUnitsByProperty/get-units-by-property.query-handler';
 import { PersistenceModule } from '@/infrastructure/persistence/persistence.module';
 
 const CommandHandlers = [CreateUnitHandler];
+const QueryHandlers = [GetUnitsByPropertyQueryHandler];
 
 @Module({
   imports: [CqrsModule, PersistenceModule],
-  providers: [...CommandHandlers],
-  exports: [...CommandHandlers],
+  providers: [...CommandHandlers, ...QueryHandlers],
+  exports: [...CommandHandlers, ...QueryHandlers],
 })
 export class UnitApplicationModule {}
