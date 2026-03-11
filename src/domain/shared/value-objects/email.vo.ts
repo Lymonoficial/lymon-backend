16f47bc2bfe4461dd1f1feb943d36c841367613b
Felipe Villa <villajaramillofelipe4@gmail.com>
2026-03-11 18:13:25 -0500

    LYMON-257 [BE] Agregar interceptor de excepciones de capa de dominio para mostrarlas en presentación.

--- src/domain/shared/value-objects/email.vo.ts
+++ src/domain/shared/value-objects/email.vo.ts
@@ -1,3 +1,5 @@
+import { DomainException } from '@/domain/shared/exceptions/domain.exception';
+
 export class Email {
   private readonly value: string;
 
@@ -7,7 +9,7 @@ export class Email {
 
   static create(email: string): Email {
     if (!this.isValid(email)) {
-      throw new Error('Invalid email format');
+      throw new DomainException('Invalid email format');
     }
     return new Email(email.toLowerCase().trim());
   }
