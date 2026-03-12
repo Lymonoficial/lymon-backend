01f9244750aafd305e6b8e0633253fefbef7b8f7
Lymonoficial <lymonoficial@outlook.com>
2026-03-12 17:05:59 -0500

    fix: Update main.ts to configure deployment

--- src/main.ts
+++ src/main.ts
@@ -87,7 +87,7 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
   logger.log(`Application running on: http://localhost:${port}`);
   logger.log(`Swagger docs: http://localhost:${port}/api/docs`);
 }
