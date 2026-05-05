86d952e9ad42112d0001e3246d66b44221f5fd19
Felipe Villa <villajaramillofelipe4@gmail.com>
2026-05-05 14:44:19 -0500

    LYMON-847 Update work shift

--- src/application/shift/shift-application.module.ts
+++ src/application/shift/shift-application.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CreateShiftCommandHandler } from '@/application/shift/commands/create-shift/create-shift.handler';
 import { UpdateShiftCommandHandler } from '@/application/shift/commands/update-shift/update-shift.handler';
+import { AssignStaffToShiftCommandHandler } from '@/application/shift/commands/assign-staff-to-shift/assign-staff-to-shift.handler';
 import { PersistenceModule } from '@/infrastructure/persistence/persistence.module';
 import { EmailModule } from '@/infrastructure/email/email.module';
 import { ShiftNotificationService } from '@/application/shift/services/shift-notification.service';
 import { ShiftAuditDiffService } from '@/domain/shift/services/shift-audit-diff.service';
 
-const CommandHandlers = [CreateShiftCommandHandler, UpdateShiftCommandHandler];
+const CommandHandlers = [
+  CreateShiftCommandHandler,
+  UpdateShiftCommandHandler,
+  AssignStaffToShiftCommandHandler,
+];
 const ApplicationServices = [ShiftNotificationService];
 const DomainServices = [ShiftAuditDiffService];
 
