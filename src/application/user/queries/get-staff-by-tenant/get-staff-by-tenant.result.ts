f701c7b4c31e13f39dc7b66b146e9bc91aff9c89
Felipe Villa <villajaramillofelipe4@gmail.com>
2026-05-12 20:18:47 -0500

    LYMON-449: refactor get endpoint to retrieve more information

--- src/application/user/queries/get-staff-by-tenant/get-staff-by-tenant.result.ts
+++ src/application/user/queries/get-staff-by-tenant/get-staff-by-tenant.result.ts
@@ -1,9 +1,27 @@
+export interface StaffScopedResourceDto {
+  id: string;
+  name: string;
+}
+
+export type StaffScopeDto =
+  | { type: 'TENANT' }
+  | {
+      type: 'PROPERTY';
+      resourceIds: string[];
+      resources: StaffScopedResourceDto[];
+    }
+  | {
+      type: 'UNIT';
+      resourceIds: string[];
+      resources: StaffScopedResourceDto[];
+    };
+
 export interface StaffDto {
   id: string;
   email: string;
   isOwner: boolean;
   emailVerified: boolean;
-  roleAssignments: Array<{ roleId: string; scope: any }>;
+  roleAssignments: Array<{ roleId: string; scope: StaffScopeDto }>;
 }
 
 export interface GetStaffByTenantResult {
